56f8c3ea9102d421aeec2d0b89a49fb0215e2d58
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2019-02-19 12:13:55 -0600

    Create prepare-environment.sh

---
+++ prepare-environment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## This script is intended to be run:
+##     on the control host (ie: workstation)
+##     CWD =  ~root/RHEL8-Workshop
+
+myInventory="./configs/rhel8-workshop"
+
+if [ ! -e "${myInventory}" ] ; then
+    echo "ERROR: Are you in the right directory? Can not find ${myInventory}" ; exit
+    exit
+fi
+
+time ansible-playbook -i ${myInventory} -f 5 \
+    ./playbooks/prep-environment.yml
+    
