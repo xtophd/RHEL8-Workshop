316d58eb21448dced6c742d9aaf9f62ca687370a
root <root@mac107b449f1cd2.lab.linuxsoup.com>
2020-02-20 11:29:59 +0000

    integrating ws-deployer-kvm to automate workshop deployment on kvm/libvirt hosts

---
+++ prepare-kvm-host.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+##
+## NOTE: you must point to the correct inventory
+##
+##   Take a sample config from ./configs and 
+##   copy it to ./playbooks/vars-custom/master-config.yml
+##
+
+myInventory="./config/master-config.yml"
+
+## This script is intended to be run:
+##     on the libvirt hypervisor node
+##     in the project directory
+##     EX: CWD == ~root/OCP4-Workshop
+
+if [ ! -e "${myInventory}" ] ; then
+    echo "ERROR: Are you in the right directory? Can not find ${myInventory}" ; exit
+    exit
+fi
+
+if [ ! -e "./playbooks" ] ; then
+    echo "ERROR: Are you in the right directory? Can not find ./playbooks" ; exit
+    exit
+fi
+
+##
+##
+##
+
+case "$1" in
+    "all")
+        time ansible-playbook -i ${myInventory} -f 10 ./playbooks.deployer-kvm/libvirt-setup.yml 
+        ;;
+         
+    "basics")
+        time ansible-playbook -i ${myInventory} -f 10 ./playbooks.deployer-kvm/libvirt-basics.yml 
+        ;;
+         
+    "cockpit")
+        time ansible-playbook -i ${myInventory} -f 10 ./playbooks.deployer-kvm/libvirt-cockpit.yml 
+        ;;
+
+    "network")
+        time ansible-playbook -i ${myInventory} -f 10 ./playbooks.deployer-kvm/libvirt-network.yml 
+        ;;
+         
+    "dns")
+        time ansible-playbook -i ${myInventory} -f 10 ./playbooks.deployer-kvm/libvirt-dns.yml 
+        ;;
+         
+    "bastion")
+        time ansible-playbook -i ${myInventory} -f 10 ./playbooks.deployer-kvm/libvirt-create-bastion.yml 
+        ;;
+
+    "nodes")
+        time ansible-playbook -i ${myInventory} -f 10 ./playbooks.deployer-kvm/libvirt-create-nodes.yml 
+        ;;
+
+    "yumconfig")
+        time ansible-playbook -i ${myInventory} -f 10 ./playbooks.deployer-kvm/libvirt-yumconfig.yml 
+        ;;
+
+    *)
+        echo "USAGE: libvirt-setup.sh [ all | basics | network | dns | bastion | nodes | yumconfig ]"
+        ;;
+
+esac         
+
