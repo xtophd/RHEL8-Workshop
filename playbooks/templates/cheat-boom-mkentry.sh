f8c04a5bcd8c5278161f77691c3fab209f45a108
Christoph Doerbeck <38790538+xtophd@users.noreply.github.com>
2020-04-19 09:49:18 -0500

    Update cheat-boom-mkentry.sh

--- playbooks/templates/cheat-boom-mkentry.sh
+++ playbooks/templates/cheat-boom-mkentry.sh
@@ -1,15 +1,28 @@
 #!/bin/bash -x
 
+# For the workshop, we need to be prepared for 3 possibilities
+#   root disk is specified by UUID
+#   root disk is a LVM
+#   root disk is a block device
+#
+# This logic appears to work until proven otherwise
+
 echo "Determining root device..."
 
-eval $(grep -o '\broot=[^ ]*' /proc/cmdline)*
+eval $(grep -o '\broot=[^ ]*' /proc/cmdline)
 
-echo "root=${root}"
-echo ""
+echo "UUID reduction if necessary..."
+
+if [[ "${root}" =~ ^UUID=(.*) ]] ; then 
+  rootdev=`blkid -U ${BASH_REMATCH[1]}`
+else
+  rootdev="${root}"
+fi
 
 echo "Creating GRUB2 entry..."
-if /usr/sbin/lvs -q ${root} ; then \
-  boom create --title "Alt Kernel Parms" --rootlv ${root}
+
+if /usr/sbin/lvs -q ${rootdev} ; then 
+  boom create --title "Alt Kernel Parms" --rootlv ${rootdev}
 else
-  boom create --title "Alt Kernel Parms" --root-device ${root}
+  boom create --title "Alt Kernel Parms" --root-device ${rootdev}
 fi
