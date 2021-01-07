dd448160fa765e0f0feecb246b41d055df25275a
root <root@deployer.lab.linuxsoup.com>
2021-01-06 20:27:13 -0600

    incorporating the newer ansible role 'xtoph_deploy' to manage automated deployments

---
+++ xtoph-deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+##
+## NOTE: you must point to the correct inventory and extravars yml
+##
+##   Take a sample configs from ./sample-configs and 
+##   copy it to ./playbooks/config/{master,libvirt}-config.yml
+##
+
+myInventory="./config/master-config.yml"
+
+if [[ ! -e "${myInventory}" || ! -e "./xtoph-deploy.yml" ]] ; then
+    echo "ERROR: Are you in the right directory? Can not find ${myInventory} | xtoph-deploy.yml" ; exit
+    exit
+fi
+
+##
+##
+##
+
+case "$1" in
+    "")
+
+        time  ansible-playbook --ask-vault-pass -i ${myInventory} -f 10 xtoph-deploy.yml
+        ;;
+
+    "deploy"     | \
+    "undeploy"   | \
+    "redeploy"   | \
+    "finish"     | \
+    "workshop"   | \
+    "setup")
+
+        time  ansible-playbook --ask-vault-pass -i ${myInventory} -f 10 --tags $1 xtoph-deploy.yml
+        ;;
+
+    *)
+        echo "USAGE: xtoph-deploy.sh [ all | setup | deploy | undeploy | redeploy | workshop ]"
+        ;;
+
+esac         
