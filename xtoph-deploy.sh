cf24cecfbb09019465574da62dcbdfd0eb6b39a3
root <root@control.rdu-ff84.internal>
2021-02-12 15:58:06 +0000

    UPDATE: moved master_config.yml to .ini because it bothered 'some' people ;-)

--- xtoph-deploy.sh
+++ xtoph-deploy.sh
@@ -4,10 +4,10 @@
 ## NOTE: you must point to the correct inventory and extravars yml
 ##
 ##   Take a sample configs from ./sample-configs and 
-##   copy it to ./playbooks/config/{master,libvirt}-config.yml
+##   copy them to ./playbooks/config
 ##
 
-myInventory="./config/master-config.yml"
+myInventory="./config/master-config.ini"
 
 if [[ ! -e "${myInventory}" || ! -e "./xtoph-deploy.yml" ]] ; then
     echo "ERROR: Are you in the right directory? Can not find ${myInventory} | xtoph-deploy.yml" ; exit
