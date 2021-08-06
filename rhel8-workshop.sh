0f67b1da2058b25aaa5f61e0b5e4fa6cede9cb00
christoph@linuxsoup.com <root@deployer.lab.linuxsoup.com>
2021-08-06 17:08:29 -0500

    + switched to master-config.yml format

--- rhel8-workshop.sh
+++ rhel8-workshop.sh
@@ -4,7 +4,7 @@
 ##     on the control host (ie: workstation)
 ##     CWD =  ~root/RHEL8-Workshop
 
-myInventory="./config/master-config.ini"
+myInventory="./config/master-config.yml"
 
 if [ ! -e "${myInventory}" ] ; then
     echo "ERROR: Are you in the right directory? Can not find ${myInventory}" ; exit
